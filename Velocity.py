be4cd6adc758bfd833d946939b41d07ebcf46e5e
Rakha98 <56153960+rakha98@users.noreply.github.com>
2019-10-16 12:51:49 +0700

    gerak lurus

---
+++ Velocity.py
@@ -0,0 +1,17 @@
+nama = 'Rakha'
+program = 'gerak lurus'
+
+print (f'program {program} oleh {nama}')
+
+def hitung_kecepatan (jarak, waktu):
+    kecepatan = jarak/waktu
+    print(f'jarak {jarak/1000}km ditempuh dalam waktu = {waktu/60}')
+    print(f'sehingga kecepatan = {kecepatan}m/s')
+    return jarak/waktu
+
+#jarak = 1000
+#waktu = 5*60
+
+kecepatan = hitung_kecepatan (1000, 5 * 60)
+kecepatan = hitung_kecepatan (3000, 70 *60)
+
